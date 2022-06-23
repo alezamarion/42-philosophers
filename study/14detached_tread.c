ff6be452eb5cd202ac69f0e2204bc92cfa0a4f64
alezamarion <alezamarion@icloud.com>
2022-06-23 14:52:49 -0300

    detached_tread

--- study/14detached_tread.c
+++ study/14detached_tread.c
@@ -13,17 +13,18 @@ void    *routine(void *args)
     printf("Finished execution\n");
 }
 
-int     main(int argc, char *argv[])
-{
-    pthread_t thread[THREAD_NUM];
-    int     i;
+//first version: here you creates the threads and then detache it, in a second version you'll create detached threads
+// int     main(int argc, char *argv[])
+// {
+//     pthread_t thread[THREAD_NUM];
+//     int     i;
 
-    for (i = 0; i < THREAD_NUM; i++)
-    {
-        if (pthread_create(&thread[i], NULL, &routine, NULL) != 0)
-            perror("Failed to create thread");
-        pthread_detach(thread[i]);
-    }
+//     for (i = 0; i < THREAD_NUM; i++)
+//     {
+//         if (pthread_create(&thread[i], NULL, &routine, NULL) != 0)
+//             perror("Failed to create thread");
+//         pthread_detach(thread[i]);
+//     }
 
 /*  when you use pthread_detach, you do not need to use pthread_join, makes no sense
     for (i = 0; i < THREAD_NUM; i++)
@@ -36,8 +37,36 @@ int     main(int argc, char *argv[])
     the threads are killed
     return (0);
 */
-    pthread_exit(0); //here threads can finish before main exits
+//    pthread_exit(0); //here threads can finish before main exits
+//}
+
+/*
+    second version: creating detached threads from beginning, instead of changing after you create an thread
+    you will use the second parameter of pthread_create(): you will need a second object, an 
+    ptread_attr_t data type, and you will have to cal pthread_attr_init() and pthread_attr_setdetachstate()
+    After all this, you'll have to destroy the object using pthread_attr_destroy()
+*/
+int     main(int argc, char *argv[])
+{
+    pthread_t       thread[THREAD_NUM];
+    pthread_attr_t  detached_thread;  
+    int             i;
+
+    pthread_attr_init(&detached_thread);
+    pthread_attr_setdetachstate(&detached_thread, PTHREAD_CREATE_DETACHED); 
+
+    for (i = 0; i < THREAD_NUM; i++)
+    {
+        if (pthread_create(&thread[i], &detached_thread, &routine, NULL) != 0)
+            perror("Failed to create thread");
+        pthread_detach(thread[i]);
+    }
+    pthread_attr_destroy(&detached_thread);
+    pthread_exit(0);
 }
+
+
+
 
 /*
     pthread_detach() used when you have a long running process you want to start, inside the main thread

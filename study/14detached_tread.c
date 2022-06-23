decd394d6375506b73c5cf06fb84e37d07c377bb
alezamarion <alezamarion@icloud.com>
2022-06-23 14:38:49 -0300

    detached_tread

--- study/14detached_tread.c
+++ study/14detached_tread.c
@@ -24,10 +24,25 @@ int     main(int argc, char *argv[])
             perror("Failed to create thread");
         pthread_detach(thread[i]);
     }
+
+/*  when you use pthread_detach, you do not need to use pthread_join, makes no sense
     for (i = 0; i < THREAD_NUM; i++)
     {
         if (pthread_join(thread[i], NULL) != 0)
             perror("Failed to join thread");
     }
+    
+    instead of using return(0), you use pthread_exit(0): using return(0), threads are on sleep(1) and the main terminates, so 
+    the threads are killed
     return (0);
+*/
+    pthread_exit(0); //here threads can finish before main exits
 }
+
+/*
+    pthread_detach() used when you have a long running process you want to start, inside the main thread
+    but you do not want to let main thread keep on running, because it will take a long time to execute all 
+    threads. So main starts threads and then forget about it.
+
+
+*/

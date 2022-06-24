6eaae3d646b01635f0f110c0fffe5342cd1c9748
alezamarion <alezamarion@icloud.com>
2022-06-25 00:13:35 +0200

    philo_atoi

--- src/utils/philos_atoi.c
+++ src/utils/philos_atoi.c
@@ -6,20 +6,20 @@
 /*   By: azamario <azamario@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 00:07:44 by azamario          #+#    #+#             */
-/*   Updated: 2022/06/25 00:07:46 by azamario         ###   ########.fr       */
+/*   Updated: 2022/06/25 00:13:18 by azamario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-double	philo_atoi(const char *nptr)
+double	philo_atoi(const char *ptr)
 {
 	double	number;
 
 	number = 0;
-	while (ft_isdigit(*nptr))
+	while (ft_isdigit(*ptr))
 	{
 		number *= 10;
-		number += (*nptr - '0');
-		nptr++;
+		number += (*ptr - '0');
+		ptr++;
 	}
 	return (number);
 }

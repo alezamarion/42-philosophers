4b2556c3e6cfeeec5b70f7b857acb4f60d6ac776
alezamarion <alezamarion@icloud.com>
2022-06-24 23:59:28 +0200

    creating structure

---
+++ src/system/philo.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: azamario <azamario@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/24 23:55:27 by azamario          #+#    #+#             */
+/*   Updated: 2022/06/24 23:58:53 by azamario         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		main(int argc, char **argv)
+{
+	if (!check_errors(argc, argv))
+		return (EXIT_FAILURE);
+	get_arguments(argc, argv, &philo_struct);
+	if (!init_simulation(&philo_struct)
+		return (EXIT_FAILURE);
+	print_end_message(&philo_struct);
+	destroy_mutexes();
+
+	return (0);
+	
+}

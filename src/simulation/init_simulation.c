0e17bce29fb06ffce4ddcac3d8a76f4aa07e1822
alezamarion <alezamarion@icloud.com>
2022-07-05 21:53:15 +0200

    forks test

--- src/simulation/init_simulation.c
+++ src/simulation/init_simulation.c
@@ -6,7 +6,7 @@
 /*   By: azamario <azamario@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 20:40:56 by azamario          #+#    #+#             */
-/*   Updated: 2022/07/05 21:20:10 by azamario         ###   ########.fr       */
+/*   Updated: 2022/07/05 21:52:17 by azamario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,14 @@ static void	define_forks(t_philo *philo, int i, long last_philo)
 {
 	philo[i].hand[LEFT] = i;				
 	philo[i].hand[RIGHT] = i + 1;
-	if (i == last_philo)
+	printf("philo %i: hand_lef: %i\n", i, philo[i].hand[LEFT]);
+	if (i == last_philo){
 		philo[i].hand[RIGHT] = 1;	//a mão direita seria 201 (no caso de 200 philos)	
+		printf("philo %i: hand_right: %i\n", i, philo[i].hand[RIGHT]);
+	}
+	else
+		printf("philo %i: hand_right: %i\n\n", i, philo[i].hand[RIGHT]);
+
 }
 
 static void	init_philos(t_philo *philo, t_dinner *dinner, long number_of_philos, int i)

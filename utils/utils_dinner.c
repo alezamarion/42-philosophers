01e9c2f285511c04a5ad64604bdb3d70e4c8fc60
alezamarion <alezamarion@icloud.com>
2022-07-08 03:31:37 +0200

    create_philo + print_status

--- utils/utils_dinner.c
+++ utils/utils_dinner.c
@@ -6,7 +6,7 @@
 /*   By: azamario <azamario@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 02:36:39 by azamario          #+#    #+#             */
-/*   Updated: 2022/07/08 02:50:03 by azamario         ###   ########.fr       */
+/*   Updated: 2022/07/08 03:30:47 by azamario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,8 @@ void	*one_philo(t_philo *philo)
 	pthread_mutex_lock(&philo->struct_data->forks[philo->right_fork]);
 	philo->last_meal = get_time();
 	print_status(get_time(), philo, "has taken a fork");
+	pthread_mutex_unlock(&philo->struct_data->forks[philo->right_fork]);
+	print_status(get_time(), philo, "DIED");
+	philo->struct_data->checker = 1;
+	return (NULL);	
 }
